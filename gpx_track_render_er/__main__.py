283f60c6e0b2aa5f15234031c2796cd55038d5b0
Ieuan Jenkins <hi@ieuan.net>
2021-01-03 13:13:24 +0000

    feat: add export with times, improve logging and refactor configuration

--- gpx_track_render_er/__main__.py
+++ gpx_track_render_er/__main__.py
@@ -5,12 +5,16 @@ from geopy import distance
 import gpxpy
 import gpxpy.gpx
 import logging
+import pytz
 import srtm
 import isodate
 import urllib.parse
 
 
 def guess_timestamp_with_acc(timestamp: str, accuracy: str = "PT0H0M0S") -> dt:
+
+    utc = pytz.utc
+
     dt_format = "%Y-%m-%dT%H:%M:%S%z"  # 2020-12-29T13:34:38+00:00
     acc_span = isodate.parse_duration(accuracy)
     start_time = dt.strptime(timestamp, dt_format) - acc_span
@@ -21,7 +25,9 @@ def guess_timestamp_with_acc(timestamp: str, accuracy: str = "PT0H0M0S") -> dt:
 
 
 def main():
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
     logging.debug("Importing configuration data")
+
     config = confuse.Configuration("gpx_track_render_er", __name__)
     config.set_file("config_default.yaml")
 
@@ -55,30 +61,38 @@ def main():
         None,
     )
 
-    print("Processing track name {0}".format(track.name))
+    logging.info("Processing track name {0}".format(track.name))
+    track.segments[0].get_nearest_location(finish_location)
+
     nearest_location = track.segments[0].points[0]
-    shortest_dist = distance.distance(meters=float("inf"))
-    for segment in track.segments:
-        for point in segment.points:
+    shortest_dist = None
+    for segment_index, segment in enumerate(track.segments):
+        for point_index, point in enumerate(segment.points):
             this_distance = distance.distance(
                 (finish_location.latitude, finish_location.longitude),
                 (point.latitude, point.longitude),
             )
 
-            print("Distance from finish: {0}".format(this_distance))
+            logging.debug("Distance from finish: %s", this_distance)
 
-            if this_distance < shortest_dist:
+            if shortest_dist is None or this_distance < shortest_dist:
                 shortest_dist = this_distance
                 nearest_location = point
+                gpx_indices = (segment_index, point_index)
 
-    print(
-        "Estimated finish at {0}. Closest GPX track point is ({1},{2}) -> {3} metres away from the finish location".format(
-            finish_time,
-            nearest_location.latitude,
-            nearest_location.longitude,
-            round(shortest_dist.m, 2),
-        )
+    logging.info("Estimated start at %s", start_time)
+    logging.info("Finish at %s", finish_time)
+    logging.info(
+        "Closest GPX track point is (%s,%s)",
+        nearest_location.latitude,
+        nearest_location.longitude,
     )
+    logging.info(
+        "%.2f metres away from the finish location and elevation Δ is +%.2f " "metres",
+        shortest_dist.m,
+        nearest_location.elevation - finish_location.elevation,
+    )
+
     gmaps_params = urllib.parse.urlencode(
         {
             "api": 1,
@@ -92,6 +106,16 @@ def main():
     gpx_output = gpxpy.gpx.GPX()
     gpx_track = gpxpy.gpx.GPXTrack()
     gpx_output.tracks.append(gpx_track)
+
+    track.split(track_segment_no=gpx_indices[0], track_point_no=gpx_indices[1])
+    gpx_track.segments.append(track.segments[0])
+    gpx_output.fill_time_data_with_regular_intervals(
+        start_time=start_time, end_time=finish_time
+    )
+    print("GPX:", gpx_output.to_xml())
+
+    with open("/home/ieuan/repos/gpx-track-render-er/data/output.gpx", "w") as f:
+        f.write(gpx_output.to_xml())
 
 
 if __name__ == "__main__":
